6564f16980bc6166954bee7be63b3d8b33e6b11b
jysaa5 <jysaa5@naver.com>
2026-04-28 22:28:26 +0900

    feat: add solution for programmers '배열의 평균값'

---
+++ Programmers/120817.js
@@ -0,0 +1,10 @@
+// 배열의 평균값
+function solution(numbers) {
+  let answer = 0;
+
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+
+  answer = sum / numbers.length;
+
+  return answer;
+}
