91cf0cbf4096688e80e5965cb0434c16ed72e397
jysaa5 <jysaa5@naver.com>
2026-05-10 01:26:55 +0900

    feat: add solutions for programmers '점의 위치 구하기'

---
+++ Programmers/120841.js
@@ -0,0 +1,13 @@
+// 점의 위치 구하기
+
+function solution(dot) {
+  let answer = 0;
+
+  if (dot[0] > 0) {
+    answer = dot[1] > 0 ? 1 : 4;
+  } else {
+    answer = dot[1] > 0 ? 2 : 3;
+  }
+
+  return answer;
+}
