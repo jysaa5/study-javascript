72480c6df07cef21447c010a95fced9f4be2ca4d
jysaa5 <jysaa5@naver.com>
2026-07-30 14:56:52 +0900

    feat: add solutions for programmers '접미사인지 확인하기'

--- Programmers/181908.js
+++ Programmers/181908.js
@@ -3,8 +3,9 @@
 function solution(my_string, is_suffix) {
   let answer = 0;
 
-  for (let i = 0; i < my_string.length - 1; i++) {
-    const suffix = my_string.slice(i + 1);
+  for (let i = 0; i < my_string.length; i++) {
+    const suffix = my_string.slice(i);
+    console.log(suffix);
     if (suffix === is_suffix) {
       answer = 1;
       return answer;
