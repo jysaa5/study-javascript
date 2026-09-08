00df0acf602aabd8cc44970a803a6780689111e7
jysaa5 <jysaa5@naver.com>
2026-09-08 23:32:48 +0900

    feat: add another solutions for programmers '타켓 넘버'

---
+++ Programmers/43165.js
@@ -0,0 +1,21 @@
+// 타겟 넘버
+
+function solution(numbers, target) {
+  let answer = 0;
+
+  function dfs(index, sum) {
+    if (index === numbers.length) {
+      if (sum === target) {
+        answer++;
+      }
+      return;
+    }
+
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
+  }
+
+  dfs(0, 0, answer);
+
+  return answer;
+}
